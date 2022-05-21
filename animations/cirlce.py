1fe01b4d76f0cef52fd59d9c0b375ab80b59bed4
xzendric <xzendric@users.noreply.github.com>
2022-05-21 10:30:34 +0200

    scaling done

--- animations/cirlce.py
+++ animations/cirlce.py
@@ -5,39 +5,60 @@ from matplotlib import pyplot as plt
 fig = plt.figure()
 ax = fig.add_subplot(projection="3d")
 
-def rotationBasedPatter(start, stop, position ,overlap, n, z):
+class rotationBasedPattern():
+    def __init__(self) -> None:
+        self.fullCircle = np.radians(360)
+        self.start :int = 1
+        self.stop :int = 1
+        self.position :int = 1
+        self.overlap :float = 1
+        self.n :int = 1
+        self.z :int = 1
 
-    fullCircle = np.radians(360)
-    if stop * fullCircle >= position >= start * fullCircle:
-        z += overlap * -fullCircle * 10 / (n * fullCircle)
-    return( z)
+    def rotationBasedPatter(self, start, stop, overlap):
+        if overlap not in range(-101,101):
+            print('wrong overlap')
+        overlap = (self.scale(overlap, (-100, 100), (2.0, 0.0)))
+        fullCircle = np.radians(360)
+        if stop * fullCircle >= self.position >= start * fullCircle:
+            self.z += -overlap * (fullCircle * 10) / (self.n * fullCircle)
+            self.overlap = overlap
+        return(self.z)
+    
+    def scale(self, val, src, dst):
+        """
+        Scale the given value from the scale of src to the scale of dst.
+        """
+        return ((val - src[0]) / (src[1]-src[0])) * (dst[1]-dst[0]) + dst[0]
 
 def gen(n):
-    phi = 0
     fullCircle = np.radians(360)
     startHeight = 10
-    downPerRevolution = 2
     z = startHeight
     rotations = 0
     endCircleCount = 10 * fullCircle
-    filmHeight = 2
+    filmHeight = 1
 
-    while phi < endCircleCount:
+    pattern = rotationBasedPattern()
+    pattern.n = n
+    pattern.z = startHeight
 
-
+    while pattern.position/2 < endCircleCount:
         
-        z = rotationBasedPatter(2,3,phi,1,n,z)
-        z = rotationBasedPatter(3,7,phi,0.4,n,z)
-        z = rotationBasedPatter(7,8,phi,2,n,z)
-        z = rotationBasedPatter(8,10,phi,0.25,n,z)
-
-        yield np.array([np.cos(phi), np.sin(phi), z])
-
-        if phi >= rotations * fullCircle:
+        z = pattern.rotationBasedPatter(start=0,stop=3,overlap=100)
+        z = pattern.rotationBasedPatter(3,7,25)
+        z = pattern.rotationBasedPatter(7,8,-100)
+        z = pattern.rotationBasedPatter(8,10,-50)
+        if not z > 0:
+            z = 0
+        yield np.array([np.cos(pattern.position), np.sin(pattern.position), z])
+        yield np.array([np.cos(pattern.position), np.sin(pattern.position), z-filmHeight])
+        if pattern.position >= rotations * fullCircle:
             rotations += 1
             print(f"Rotation {rotations}: z = {z:.2f}")
+            print(pattern.overlap)
 
-        phi += endCircleCount / n
+        pattern.position += endCircleCount / pattern.n
         # print(rotations, z)
 
 
@@ -63,7 +84,7 @@ ax.set_zlim3d([0.0, 10.0])
 ax.set_zlabel("Z")
 
 ani = animation.FuncAnimation(
-    fig, update, frames=N, fargs=(data, line), interval=0.2, blit=False
+    fig, update, frames=N*2, fargs=(data, line), interval=0.2, blit=False
 )
 # ani.save('matplot003.gif', writer='imagemagick', fps=60)
 plt.show()
