b7dc840c5f609ac82aa099095e1802f24f6becb2
xzendric <xzendric@users.noreply.github.com>
2022-05-23 20:04:48 +0200

    working

--- animations/cirlce.py
+++ animations/cirlce.py
@@ -25,7 +25,7 @@ class wrappingPattern:
     def rotationBased(self, startRotation, stopRotation, overlap):
         if overlap not in range(-101, 101):
             raise ExceptionOutOfRange("Overlap out of range.")
-        self.overlap = self.scale(overlap, (-100, 100), (self.filmHeight * 2, 0.0))
+        self.overlap = self.scale(overlap, (-100, 100), (self.filmHeight * 2, 0))
         fullCircle = np.radians(360)
         if stopRotation * fullCircle >= self.position >= startRotation * fullCircle:
             self.PosZAdjustment = -self.overlap * (fullCircle * 10) / (self.n * fullCircle)
@@ -54,7 +54,7 @@ class wrappingPattern:
         self.overlap = self.scale(overlap, (-100, 100), (self.filmHeight * 2, 0.0))
         fullCircle = np.radians(360)
 
-        if start  >= self.PosZ >= stop:
+        if productHeight >= start >= self.PosZ >= stop:
             self.PosZAdjustment = -self.overlap * (fullCircle * 10) / (self.n * fullCircle)
             return(True)
 
@@ -68,11 +68,11 @@ class wrappingPattern:
 
 def gen(n):
     Circle = np.radians(360)
-    productHeight = 5
+    productHeight = 500
     PosZ = productHeight
     rotations = 0
     endCircleCount = 10 * Circle
-    filmHeight = 0.5
+    filmHeight = 50
 
     pattern = wrappingPattern()
     pattern.n = n
@@ -83,18 +83,15 @@ def gen(n):
     # TODO choose between rotation based or height based.
     while pattern.position < endCircleCount:
         # TODO make sure only one pattern is active at a time.
-        a = pattern.rotationBased(startRotation=0, stopRotation=2, overlap=100)
-        b = pattern.rotationBased(2, 4, 0)
-        c = pattern.measurementBased(productHeight,4.01,2.0,-50)
-        d = pattern.percentageBased(productHeight, 80, 100, -100)
-
-        PosZ += pattern.PosZAdjustment
-        pattern.PosZ = PosZ
+        a = pattern.rotationBased(startRotation=0, stopRotation=1, overlap=100)
+        b = pattern.rotationBased(1, 4, 0)
+        c = pattern.measurementBased(productHeight,350,200,-50)
+        d = pattern.percentageBased(productHeight, 60, 100, -100)
 
         if not PosZ > 0 + filmHeight:
             PosZ = 0 + filmHeight
-        PosX = np.cos(pattern.position)
-        PosY = np.sin(pattern.position)
+        PosX = np.cos(pattern.position)*200
+        PosY = np.sin(pattern.position)*200
         if yieldControl:
             yield np.array([PosX, PosY, PosZ])
             yield np.array([PosX, PosY, PosZ - filmHeight])
@@ -107,8 +104,11 @@ def gen(n):
         if pattern.position >= rotations * Circle:
             rotations += 1
             print(f"Rotation {rotations}: PosZ = {PosZ:.2f}")
-           # print(f'Control: a{a}, b{b}, c{c}, d{d},')
+            print(f'Control: a{a}, b{b}, c{c}, d{d},')
 
+        PosZ += pattern.PosZAdjustment
+        pattern.PosZ = PosZ
+        pattern.PosZAdjustment = 0
         pattern.position += endCircleCount / pattern.n
         # print(rotations, PosZ)
 
@@ -125,13 +125,13 @@ data = np.array(list(gen(N))).T
 (line,) = ax.plot(data[0, 0:1], data[1, 0:1], data[2, 0:1], lw=1, color="b")
 
 # Setting the axes properties
-ax.set_xlim3d([-2.0, 2.0])
+ax.set_xlim3d([-200, 200])
 ax.set_xlabel("X")
 
-ax.set_ylim3d([-2.0, 2.0])
+ax.set_ylim3d([-200, 200])
 ax.set_ylabel("Y")
 
-ax.set_zlim3d([0.0, 5.0])
+ax.set_zlim3d([0, 500])
 ax.set_zlabel("Z")
 
 ani = animation.FuncAnimation(
